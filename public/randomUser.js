b01df90b0e214f5b8702fcc5f3f6ff0330929bdd
Hailey Kim <haileykim@MacBook-Pro-3.local>
2021-05-23 01:34:44 -0700

    user photo array list fixed

--- public/randomUser.js
+++ public/randomUser.js
@@ -1,16 +1,12 @@
 function randomUser(dislike, like, users) {
   let second_user = users[Math.floor(Math.random() * users.length)];
-  if (
-    dislike.length >= users.length ||
-    like.length >= users.length ||
-    like.length + dislike.length >= users.length
-  ) {
+  if (like.length + dislike.length >= users.length) {
     return false;
   } else {
     while (dislike.includes(second_user.id) || like.includes(second_user.id)) {
       second_user = users[Math.floor(Math.random() * users.length)];
     }
-    return second_user;    
+    return second_user;
   }
 }
 
